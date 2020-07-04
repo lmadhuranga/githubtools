2f5a9ac1cedb00daf69ce037f11ac6f488231375
lmadhuranga <lilan.maduranga@gmail.com>
2020-07-04 10:15:24 +0530

    :gem:  init :gem:

---
+++ cbranch.sh
@@ -0,0 +1,22 @@
+# read input
+read -p 'String to dash String: ' str
+
+str=${str:-0} 
+
+# Commit and push
+if [ "$str" == 0 ]
+then 
+   echo "Please provide name for branch"
+   exit
+fi
+
+# convert to space to dash
+dashedString=$(sed "s/ /_/g" <<< $str)
+
+git stash 
+
+git checkout master  && git pull origin master
+
+git checkout -b dashedString
+
+# git push origin dashedString
