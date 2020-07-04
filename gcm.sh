5287fbc06a0a1d496d613c3d3827b959af251879
lmadhuranga <lilan.maduranga@gmail.com>
2020-07-04 21:08:14 +0530

    :pushpin: update icon list

--- gcm.sh
+++ gcm.sh
@@ -1,5 +1,30 @@
 # # type of commit
-emjArr=( [0]=':bug:' [1]=':bulb:' [2]=':hammer:' [3]=':moneybag:' [4]=':star:' [5]=':fire:' [6]=':cloud:' [7]=':snowflake:' [8]=':volcano:', [9]=':heavy_check_mark:' )
+emjArr=(
+   [0]=':bug:'
+   [1]=':bulb:'
+   [2]=':hammer:'
+   [3]=':moneybag:'
+   [4]=':star:'
+   [5]=':fire:'
+   [6]=':cloud:'
+   [7]=':snowflake:'
+   [8]=':volcano:'
+   [9]=':heavy_check_mark:'
+   [10]=':ghost:'
+   [11]=':pushpin:'
+   ['bug']=':bug:'
+   ['idea']=':bulb:'
+   ['fix']=':hammer:'
+   ['mondy']=':moneybag:'
+   ['star']=':star:'
+   ['fire']=':fire:'
+   ['cloud']=':cloud:'
+   ['snow']=':snowflake:'
+   ['volcano']=':volcano:'
+   ['check']=':heavy_check_mark:'
+   ['ghost']=':ghost:'
+   ['pin']=':pushpin:'
+)
 
 for i in "${!emjArr[@]}"; do
    echo "$i  ${emjArr[$i]}";
