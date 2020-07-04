016aa02b9df364aa2d517f286ad8f289abee1a4b
lmadhuranga <lilan.maduranga@gmail.com>
2020-07-04 21:57:49 +0530

    :hammer: update sh file with command lines

--- gcb.sh
+++ gcb.sh
@@ -1,3 +1,40 @@
+# # emojiType of commit
+emjArr=(
+   [0]=':bug:'
+   [1]=':bulb:'
+   [2]=':hammer:'
+   [3]=':moneybag:'
+   [4]=':star:'
+   [5]=':fire:'
+   [6]=':cloud:'
+   [7]=':snowflake:'
+   [8]=':volcano:'
+   [9]=':heavy_check_mark:'
+   [10]=':ghost:'
+   [11]=':pushpin:'
+   ['bug']=':bug:'
+   ['idea']=':bulb:'
+   ['fix']=':hammer:'
+   ['mondy']=':moneybag:'
+   ['star']=':star:'
+   ['fire']=':fire:'
+   ['cloud']=':cloud:'
+   ['snow']=':snowflake:'
+   ['volcano']=':volcano:'
+   ['check']=':heavy_check_mark:'
+   ['ghost']=':ghost:'
+   ['pin']=':pushpin:'
+)
+
+for i in "${!emjArr[@]}"; do
+   echo "$i  ${emjArr[$i]}";
+done
+
+read -p '(0 bug) :' emojiType
+
+# default value set 
+emojiType=${emojiType:-0} 
+
 # read input
 read -p 'String to dash String: ' str
 
@@ -26,8 +63,8 @@ echo "Linked $dashedString -> $(date) " >> $filename
 
 git add .
 
-git commit -am " :tada: init :balloon:"
+git commit -am " :tada: init $emjArr[emojiType]"
 
 git push origin $dashedString
 
-gh pr create -t "$str" -b "Linked :link: branch $dashedString"
+gh pr create -t "$str $emjArr[emojiType]" -b "Linked :link: branch $dashedString"
--- gcpr.sh
+++ gcpr.sh
@@ -1,5 +1,43 @@
+# emojiType of commit
+emjArr=(
+   [0]=':bug:'
+   [1]=':bulb:'
+   [2]=':hammer:'
+   [3]=':moneybag:'
+   [4]=':star:'
+   [5]=':fire:'
+   [6]=':cloud:'
+   [7]=':snowflake:'
+   [8]=':volcano:'
+   [9]=':heavy_check_mark:'
+   [10]=':ghost:'
+   [11]=':pushpin:'
+   ['bug']=':bug:'
+   ['idea']=':bulb:'
+   ['fix']=':hammer:'
+   ['mondy']=':moneybag:'
+   ['star']=':star:'
+   ['fire']=':fire:'
+   ['cloud']=':cloud:'
+   ['snow']=':snowflake:'
+   ['volcano']=':volcano:'
+   ['check']=':heavy_check_mark:'
+   ['ghost']=':ghost:'
+   ['pin']=':pushpin:'
+)
+
+for i in "${!emjArr[@]}"; do
+   echo "$i  ${emjArr[$i]}";
+done
+
+read -p '(0 bug) :' emojiType
+
+# default value set 
+emojiType=${emojiType:-0} 
+
+
 currentBranch=$(git branch --show-current)
 
 git push origin $currentBranch
 
-gh pr create -t "$currentBranch" -b "Linked :link: branch $currentBranch"
+gh pr create -t "$currentBranch ${emojiType}" -b "Linked :link: branch $currentBranch"
